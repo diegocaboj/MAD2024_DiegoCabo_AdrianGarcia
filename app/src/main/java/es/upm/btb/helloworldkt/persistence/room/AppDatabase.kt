d2c9c4b6cef8ec4afe4959cd9263f5d85af15763
Diego <d.cabo@alumnos.upm.es>
2024-05-29 16:17:54 +0200

    Upload Complete Kotlin Project

---
+++ app/src/main/java/es/upm/btb/helloworldkt/persistence/room/AppDatabase.kt
@@ -0,0 +1,8 @@
+package es.upm.btb.helloworldkt.persistence.room
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [LocationEntity::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun locationDao(): ILocationDao
+}
