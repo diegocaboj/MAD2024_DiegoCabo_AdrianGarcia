b902012740acae0a28c6363c1a7d4b8235bb32c0
Diego <d.cabo@alumnos.upm.es>
2024-05-29 15:49:04 +0200

    Delete master content

--- app/src/main/java/es/upm/btb/helloworldkt/persistence/room/AppDatabase.kt
+++
@@ -1,8 +0,0 @@
-package es.upm.btb.helloworldkt.persistence.room
-import androidx.room.Database
-import androidx.room.RoomDatabase
-
-@Database(entities = [LocationEntity::class], version = 1)
-abstract class AppDatabase : RoomDatabase() {
-    abstract fun locationDao(): ILocationDao
-}
